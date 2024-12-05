6c2e6c59ade73d139b921f73bf62fd67a6cc6e9d
Kevin Bader <kevinbader@users.noreply.github.com>
2024-12-05 22:01:16 +0100

    day3 part2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -5,15 +5,25 @@ use regex::Regex;
 fn main() {
     let data = fs::read_to_string(args().nth(1).unwrap_or("input/day3.txt".into())).unwrap();
 
-    let mul_regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let sum_of_products: u32 = mul_regex
-        .captures_iter(&data)
-        .map(|c| {
-            let a: u32 = c[1].parse().expect("integer");
-            let b: u32 = c[2].parse().expect("integer");
-            a * b
-        })
-        .sum();
+    let mul_regex = Regex::new(r"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut sum_of_products: u32 = 0;
+    let mut is_enabled = true;
+    for capture in mul_regex.captures_iter(&data) {
+        match &capture[0] {
+            "do()" => {
+                is_enabled = true;
+            }
+            "don't()" => {
+                is_enabled = false;
+            }
+            _ if is_enabled => {
+                let a: u32 = capture[1].parse().expect("integer");
+                let b: u32 = capture[2].parse().expect("integer");
+                sum_of_products += a * b;
+            }
+            _ => {}
+        }
+    }
 
     println!("Sum or products: {sum_of_products}");
 }
